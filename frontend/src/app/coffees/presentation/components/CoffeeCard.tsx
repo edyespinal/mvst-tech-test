a6d2637ec879eb80adc7abf0bb385fde5ac3b917
Edy Espinal <edyespinal@gmail.com>
2022-11-17 20:43:29 +0100

    update CoffeCard component

--- frontend/src/app/coffees/presentation/components/CoffeeCard.tsx
+++ frontend/src/app/coffees/presentation/components/CoffeeCard.tsx
@@ -15,12 +15,12 @@ export const CoffeeCard: React.FC<CoffeeCardProps> = ({
   description,
 }) => {
   return (
-    <div className='bg-white border border-solid border-stone-200 rounded-lg p-4'>
-      <div className='relative w-full h-64'>
-        <Image src={imageUrl} layout='fill' alt={title} />
+    <div className='bg-white border border-solid border-grey rounded-lg'>
+      <div className='relative w-full h-64 px-10 py-4 grid place-items-center'>
+        <Image src={imageUrl} height={248} width={248} alt={title} />
       </div>
 
-      <div className='border-t'>
+      <div className='border-t pt-2 px-4 pb-4'>
         <h1 className='font-semibold text-lg text-primary'>{title}</h1>
         <p className='mt-2 text-sm text-grey'>{description}</p>
       </div>
