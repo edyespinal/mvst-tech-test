fddcd45b8bcefa7a2ffca60f0f9f431cd7bc968c
Edy Espinal <edyespinal@gmail.com>
2022-11-22 12:44:35 +0100

    bye code-challenge folder

--- backend/src/code-challenge/code-challenge.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CodeChallengeController } from './code-challenge.controller';
-
-@Module({
-  controllers: [CodeChallengeController],
-  providers: [],
-})
-export class CodeChallengeModule {}
